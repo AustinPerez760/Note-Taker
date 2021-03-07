45b8fd62438c22c09b89d76ee9a83b37ba0ab9ee
Austin Perez <austin.perez760@gmail.com>
2021-03-07 01:48:08 -0800

    fixed package.json file main as server.js, filled files with starter code

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+// Sets up Express
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+//app.use(express.static("public"));
+
+// Routes
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+// Initiates server
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
